e33f5c9584a641c3e411c022b9fbb9090e43ee6a
manalipotekar03 <manali.potekar@infuse.it>
2023-08-30 01:37:21 +0530

    Added test cases in testing .rb file

--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,6 +1,1 @@
-
-  def test_find_elements_returns_mock_elements
-    results = @wrapper.find_elements(:css, '#navbarNavAltMarkup > div:nth-child(1)')
-    assert_equal('Home', results[0].text)
-    assert_equal('About Us', results[2].text)
-  end
+non_existent_element
--- testing.rb
+++ testing.rb
@@ -15,10 +15,118 @@ class TestWrapper < Test::Unit::TestCase
     @wrapper.quit
   end
 
+  def test_mouse_actions_instance_created
+    mouse_actions = MouseActions.new(@driver) 
+    assert_instance_of(MouseActions, mouse_actions)
+  end
+  
+  def test_keyboard_actions_instance_created
+    keyboard_actions = KeyboardActions.new(@driver) 
+    assert_instance_of(KeyboardActions, keyboard_actions)
+  end
+  
+  def test_element_actions_instance_created
+    element_actions = ElementActions.new(@driver)
+    assert_instance_of(ElementActions, element_actions)
+  end
+
+  
+  def test_click
+    login_link = @wrapper.find_element(:xpath, '//*[@id="navbarNavAltMarkup"]/div[2]/a[1]')
+    @wrapper.click(login_link)
+    assert_equal('https://practise.usemango.co.uk/login', @wrapper.current_url)
+  end
+
+  def test_send_keys_to_input_field
+    @keyboard_actions = KeyboardActions.new(@driver)
+    login_link = @wrapper.find_element(:xpath, '//*[@id="navbarNavAltMarkup"]/div[2]/a[1]')
+    @wrapper.click(login_link)
+    username_input = @wrapper.find_element(:id, 'exampleInputEmail1')
+    @keyboard_actions.send_keys(username_input,'testuser')
+    assert_equal('testuser', username_input.attribute('value'))
+    password_input = @wrapper.find_element(:id, 'exampleInputPassword1')
+    @keyboard_actions.send_keys(password_input,'123')
+    assert_equal('123', password_input.attribute('value'))
+  end
+
+
+  def test_get
+    assert_equal('https://practise.usemango.co.uk/', @wrapper.current_url, 'Navigation failed')
+  end
+  
+  def test_current_url_returns_correct_url
+    assert_equal('https://practise.usemango.co.uk/', @wrapper.current_url)
+  end
+
+  
+  def test_text
+    @element_actions = ElementActions.new(@driver)
+    element=@wrapper.find_element(:css, '#root > nav > a')
+    result = @element_actions.text(element)
+    assert_equal('useMango DemoWebsite', result)
+  end
+
   def test_find_element
     result = @wrapper.find_element(:css, '#root > nav > a')
+    assert_not_nil(result)
     assert_equal('useMango DemoWebsite', result.text)
   end
+
+  def test_invalid_url
+    assert_raise(Selenium::WebDriver::Error::InvalidArgumentError) do
+      @wrapper.get('bhjvh')
+    end
+  end
+
+  def test_element_not_present
+    assert_raise(Selenium::WebDriver::Error::NoSuchElementError) do
+      @wrapper.find_element(:id, 'id1234')
+    end
+  end
+
+  def test_empty_selector
+    assert_raise(Selenium::WebDriver::Error::InvalidSelectorError) do
+      element = @wrapper.find_element(:id, nil)
+    end
+  end
+ 
+  def no_such_element_present
+    assert_raise(Selenium::WebDriver::Error::InvalidArgumentError) do
+      element = @wrapper.find_element(:id, 'mobiles')
+    end
+    assert_raise(Selenium::WebDriver::Error::InvalidArgumentError) do
+      element = @wrapper.find_element(:class, 'dvd')
+    end
+  end
+ 
+
+  def test_keyboard_actions_on_readonly_field
+    @keyboard_actions = KeyboardActions.new(@driver)
+    field = @wrapper.find_element(:css, '#root > nav > a')
+    input_field = @wrapper.find_element(:css, '#root > nav > a')
+    input_text = "Hello, World!"
+    input_field.send_keys(input_text)
+    actual_value = input_field.attribute('value')
+    assert_nil(actual_value)
+  end
+  
+  def test_move_to_with_coordinates
+    @mouse_actions = MouseActions.new(@wrapper.instance_variable_get(:@driver))
+    element = @wrapper.find_element(:css, '#root > nav > a') # Replace with your element locator
+    x = 80
+    y = 200
+    result = @mouse_actions.move_to(element, x, y)
+    assert_nil(result)
+  end
+
+  def test_move_to_without_coordinates
+    @mouse_actions = MouseActions.new(@wrapper.instance_variable_get(:@driver))
+    element = @wrapper.find_element(:css, '#root > nav > a') # Replace with your element locator
+    assert_nothing_raised do
+      @mouse_actions.move_to(element, nil, nil)
+    end
+  end
+  
 
   def test_find_elements
     
@@ -32,39 +140,10 @@ class TestWrapper < Test::Unit::TestCase
     # assert_equal('About Us', results[2].text)
   end
 
-  def test_click
-    login_link = @wrapper.find_element(:xpath, '//*[@id="navbarNavAltMarkup"]/div[2]/a[1]')
-    @wrapper.click(login_link)
-    assert_equal('https://practise.usemango.co.uk/login', @wrapper.current_url)
-  end
-
-  def test_send_keys_to_input_field
-    login_link = @wrapper.find_element(:xpath, '//*[@id="navbarNavAltMarkup"]/div[2]/a[1]')
-    @wrapper.click(login_link)
-    username_input = @wrapper.find_element(:id, 'exampleInputEmail1')
-    username_input.send_keys('testuser')
-    assert_equal('testuser', username_input.attribute('value'))
-    password_input = @wrapper.find_element(:id, 'exampleInputPassword1')
-    password_input.send_keys('testuser')
-    assert_equal('testuser', password_input.attribute('value'))
-    
-  end
-
-  def test_find_element
-    login_link = @wrapper.find_element(:xpath, '//*[@id="navbarNavAltMarkup"]/div[2]/a[1]')
-    @wrapper.click(login_link)
-    some_element = @wrapper.find_element(:id, 'exampleInputPassword1')
-    assert_not_nil(some_element)
-
-  end
-
-  def test_get
-    assert_equal('https://practise.usemango.co.uk/', @wrapper.current_url, 'Navigation failed')
-  end
+  # def test_quit_method
+  #   quit_result = @wrapper.quit
+  #   assert_nil(quit_result)
+  # end
   
-  def test_current_url_returns_correct_url
-    assert_equal('https://practise.usemango.co.uk/', @wrapper.current_url)
-  end
-
 
 end
